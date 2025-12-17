983118059c5010a3577aedaed6220e7309d222a3
moharidy05 <mohamedharidy12123@gmail.com>
2025-12-17 17:11:40 +0200

    hola

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int sum(int a, int b, int c) {
+  return a + b + c;
+}
+
+
+
+int main() {
+  int a, b, c;
+  cin >> a, b, c;
+  cout << sum(a, b, c) << endl;
+  return 0;
+}
